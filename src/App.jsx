872a072e0a72f44adab50a41db3e893b74d08f5a
sidramwaseem <sidraw24@gmail.com>
2022-05-28 00:27:56 +0500

    Played around with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import All from "./components/All.jsx";
+import Active from "./components/Active.jsx";
+import Completed from "./components/Completed.jsx";
 
 function App() {
   return (
     <div>
       <Router>
+
+      <Link to="/" >All</Link>
+      <Link to="/active">Active</Link>
+      <Link to="/completed" >Completed</Link>
+
         <Routes>
-          <Route path="/"></Route>
+          <Route path="/:usernames" element={ <All/> } ></Route>
+          <Route path="/active" element={ <Active/> } ></Route>
+          <Route path="/completed" element={ <Completed/> } ></Route>
         </Routes>
       </Router>
     </div>
