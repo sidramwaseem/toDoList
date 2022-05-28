20fdfa89f723d28b61236af10287dd17b4608329
sidramwaseem <sidraw24@gmail.com>
2022-05-28 10:25:43 +0500

    Update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import All from "./components/All.jsx";
 import Active from "./components/Active.jsx";
 import Completed from "./components/Completed.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-
-      <Link to="/" >All</Link>
-      <Link to="/active">Active</Link>
-      <Link to="/completed" >Completed</Link>
-
+    <div className="App">
+      <Router className="App_Router">
+        <h1 className="App_Title">#justDo-It</h1>
+        <nav className="App_Nav">
+          <Link to="/all" className="App_Nav_Link">All</Link>
+          <Link to="/active" className="App_Nav_Link">Active</Link>
+          <Link to="/completed" className="App_Nav_Link">Completed</Link>
+        </nav>
         <Routes>
-          <Route path="/:usernames" element={ <All/> } ></Route>
-          <Route path="/active" element={ <Active/> } ></Route>
-          <Route path="/completed" element={ <Completed/> } ></Route>
+          <Route path="/:usernames" element={<All />}></Route>
+          <Route path="/active" element={<Active />}></Route>
+          <Route path="/completed" element={<Completed />}></Route>
         </Routes>
+        <footer>
+          <p>
+            Crated by{" "}
+            <a
+              href="https://github.com/sidramwaseem"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sidra
+            </a>{" "}
+            - devChallenges.io{" "}
+          </p>
+        </footer>
       </Router>
     </div>
   );
