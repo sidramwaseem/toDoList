7487b7d9085d7b8d15fd251ab159b6eb0f8167a4
sidramwaseem <sidraw24@gmail.com>
2022-05-29 11:55:01 +0500

    Fixed paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import All from "./components/All.jsx";
-import Active from "./components/Active.jsx";
-import Completed from "./components/Completed.jsx";
+import All from "./components/All/All.jsx";
+import Active from "./components/Active/Active.jsx";
+import Completed from "./components/Completed/Completed";
 import "./App.css";
 
 function App() {
