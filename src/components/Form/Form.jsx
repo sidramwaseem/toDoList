22c2db877977d36734e2c41b5c5b408d87602e8a
sidramwaseem <sidraw24@gmail.com>
2022-05-29 11:58:05 +0500

    Created separate component for Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Form.css";
+
+function Form() {
+  return (
+    <div className="Form-container">
+    <form className="Form-container_form" >
+      <input type="text" placeholder="Enter a Task" />
+      <button type="button">Add</button>
+    </form>
+  </div>
+  )
+}
+
+export default Form
