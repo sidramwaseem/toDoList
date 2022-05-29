b7a3dd2a6ab4e622a44fe1ac7553e9fda9516c01
sidramwaseem <sidraw24@gmail.com>
2022-05-29 11:56:44 +0500

    Created Completed.css

---
+++ src/components/All/All.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./All.css";
+import Form from "../Form/Form.jsx";
+// import {useParams} from 'react-router-dom';
+
+function All() {
+  return (
+    <div className="All-container">
+    <Form />
+    </div>
+  );
+}
+
+export default All;
