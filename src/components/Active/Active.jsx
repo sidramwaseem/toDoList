e74a62fb93f298fb05792467506efdb357190926
sidramwaseem <sidraw24@gmail.com>
2022-05-29 11:56:21 +0500

    Rendered Form component on Active.jsx

---
+++ src/components/Active/Active.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "../Form/Form.jsx";
+
+function Active() {
+  return (
+    <div className="Active-container">
+      <Form />
+    </div>
+  );
+}
+
+export default Active;
