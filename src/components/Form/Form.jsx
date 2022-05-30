2e3203d34eeb29f0d6a362e3b9d78aac33819d63
sidramwaseem <sidraw24@gmail.com>
2022-05-30 22:10:55 +0500

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,19 +3,11 @@ import "./Form.css";
 
 
 function Form() {
-  const [task, setTask] = React.useState("");
-
-  function addTask(event){
-    setTask(event.target.value)
-  }
-
-
-
 
   return (
     <div className="Form-container">
     <form className="Form-container_form" >
-      <input type="text" placeholder="Enter a Task" name="task" onChange={addTask}/>
+      <input type="text" placeholder="Enter a Task" name="task"/>
       <button type="button">Add</button>
     </form>
   </div>
