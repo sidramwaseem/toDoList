7f66db934480baa458ff15bc9b5947448c92b5e8
sidramwaseem <sidraw24@gmail.com>
2022-05-31 21:33:42 +0500

    Added props

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import "./Form.css";
 
 
-function Form() {
+function Form(props) {
 
 
   return (
     <div className="Form-container">
     <form className="Form-container_form" >
-      <input type="text" placeholder="Enter a Task" name="task"/>
+      <input type="text" placeholder="Enter a Task" name="task" onChange={props.handleChange}/>
       <button type="button">Add</button>
     </form>
   </div>
