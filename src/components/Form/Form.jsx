8f3a4b9aeed72f7297e484e7b65b16ece4776652
sidramwaseem <sidraw24@gmail.com>
2022-05-31 22:41:45 +0500

    Update form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,7 +8,7 @@ function Form(props) {
     <div className="Form-container">
     <form className="Form-container_form" >
       <input type="text" placeholder="Enter a Task" name="task" onChange={props.handleChange}/>
-      <button type="button">Add</button>
+      <button type="button" onClick={props.handleClick} >Add</button>
     </form>
   </div>
   )
