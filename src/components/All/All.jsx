eb6b70d9d36597a5c8c7d938fcce026cbf3403ca
sidramwaseem <sidraw24@gmail.com>
2022-05-31 23:32:57 +0500

    Update All.jsx

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./All.css";
 import Form from "../Form/Form.jsx";
-import Task from "../Task/Task.jsx";
+// import Task from "../Task/Task.jsx";
 // import {useParams} from 'react-router-dom';
 
 function All(props) {
   return (
     <div className="All-container">
-      <Form handleChange={props.handleChange} handleClick={props.handleClick} />
-      <Task />
+      <Form handleChange={props.handleChange} />
+      <ul className="all_task-list">
+        {props.task.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" name="checkbox" id={task.id} />
+            <label>{task.text}</label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
