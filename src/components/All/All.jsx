efd36ac66ddfb602e5a8ebb726a6f6da27f5b042
sidramwaseem <sidraw24@gmail.com>
2022-05-31 22:41:35 +0500

    shifted task list to its own component

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import "./All.css";
 import Form from "../Form/Form.jsx";
+import Task from "../Task/Task.jsx"
 // import {useParams} from 'react-router-dom';
 
 function All(props) {
-  console.log(props.task)
   return (
     <div className="All-container">
-      <Form handleChange={props.handleChange} />
-      <ul className="all_task-list">
-        <li>
-          <input type="checkbox" id="task1"/>
-          <label htmlFor="task1">{props.task}</label>
-        </li>
-      </ul>
+      <Form handleChange={props.handleChange} handleClick={props.handleClick} />
+      <Task />
     </div>
   );
 }
