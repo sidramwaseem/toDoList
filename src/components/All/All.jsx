aaff403b84a471ec9c8149d2a2ef57f1f3c10cf2
sidramwaseem <sidraw24@gmail.com>
2022-05-31 21:33:35 +0500

    Updated props

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -4,13 +4,13 @@ import Form from "../Form/Form.jsx";
 // import {useParams} from 'react-router-dom';
 
 function All(props) {
-  console.log(props.task);
+  console.log(props.task)
   return (
     <div className="All-container">
-      <Form />
+      <Form handleChange={props.handleChange} />
       <ul className="all_task-list">
         <li>
-          <input type="checkbox" id="task1" onChange={props.handleChange}/>
+          <input type="checkbox" id="task1"/>
           <label htmlFor="task1">{props.task}</label>
         </li>
       </ul>
