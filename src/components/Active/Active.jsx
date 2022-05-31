9443f2ce96664128ec42851d615b3ad9b1150d98
sidramwaseem <sidraw24@gmail.com>
2022-05-31 22:41:01 +0500

    Added Task component

--- src/components/Active/Active.jsx
+++ src/components/Active/Active.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "../Form/Form.jsx";
+import Task from "../Task/Task.jsx"
 
 function Active(props) {
   return (
     <div className="Active-container">
       <Form handleChange={props.handleChange}/>
+      <Task />
     </div>
   );
 }
