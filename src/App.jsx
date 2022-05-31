e22e136505db98168c1e0c991a2fc9b363259336
sidramwaseem <sidraw24@gmail.com>
2022-05-31 23:32:17 +0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import Completed from "./components/Completed/Completed";
 import "./App.css";
 
 function App() {
-  const [task, setTask] = React.useState([""]);
+  const [task, setTask] = React.useState([
+    {
+      id: 1,
+      text: "Go for Jogging",
+    },
+    {
+      id: 2,
+      text: "Do Assignment",
+    },
+  ]);
 
   function handleChange(event) {
     console.log(event.target.value);
     setTask(event.target.value);
   }
 
-  function addTask() {
-    const tempTask = [...task];
-    tempTask.push(setTask);
 
-    setTask(tempTask);
-  }
 
   return (
     <div className="App">
@@ -39,7 +43,7 @@ function App() {
           <Route
             path="/all"
             element={
-              <All task={task} handleChange={handleChange} addTask={addTask} />
+              <All task={task} handleChange={handleChange}/>
             }
           ></Route>
           <Route
