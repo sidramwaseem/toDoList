4d16ed4f14093565c8fc92873667add5f95ce9db
sidramwaseem <sidraw24@gmail.com>
2022-06-01 23:13:53 +0500

    Update All.jsx

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import "./All.css";
 import Form from "../Form/Form.jsx";
-// import Task from "../Task/Task.jsx";
-// import {useParams} from 'react-router-dom';
+import TaskList from "../TaskList/TaskList.jsx"
 
-function All() {
+function All(props) {
   return (
     <div className="All-container">
-      <Form />
-      <ul className="all_task-list">
-          <li>
-            <input type="checkbox" name="checkbox" id="1"/>
-            <label htmlFor="1" >Go for Shopping</label>
-          </li>
-      </ul>
+      <Form task={props.task} addTask={props.addTask}/>
+      <TaskList task={props.task} />
     </div>
   );
 }
