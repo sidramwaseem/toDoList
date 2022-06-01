5a417539b4108b6ac1a96e8f3c85d871b5bc81d7
sidramwaseem <sidraw24@gmail.com>
2022-06-01 23:14:18 +0500

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Form.css";
 
-function Form() {
+function Form(props) {
+
+
   return (
     <div className="Form-container">
-      <form className="Form-container_form">
+      <form className="Form-container_form" onSubmit={props.addItem} >
         <input
           type="text"
           placeholder="Enter a Task"
           name="task"
+          id="isDone"
+          checked={props.isDone}
         />
         <button type="button">
           Add
