42fdc05b7d67d20db4428b62ef8b72d5de888196
sidramwaseem <sidraw24@gmail.com>
2022-06-01 23:52:03 +0500

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,19 +4,38 @@ import "./Form.css";
 function Form(props) {
 
 
+  const [form, setFormData] = React.useState("")
+
+  function handleChange(e){
+    setFormData(prevData => e.target.value )
+  }
+
+  function addItem(event) {
+    if (form !== "") {
+      var newItem = {
+        text: form,
+        id: Date.now() + "" + Math.floor(Math.random() * 78),
+        key: Date.now(),
+        isDone: false,
+      };
+      props.addTask(newItem);
+    }
+    event.preventDefault();
+  }
+
   return (
     <div className="Form-container">
-      <form className="Form-container_form" onSubmit={props.addItem} >
+      <form className="Form-container_form" onSubmit={addItem}>
         <input
+          onChange={handleChange}
           type="text"
           placeholder="Enter a Task"
-          name="task"
+          name="text"
           id="isDone"
           checked={props.isDone}
+          value={form}
         />
-        <button type="button">
-          Add
-        </button>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
