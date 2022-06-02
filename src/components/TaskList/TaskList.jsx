3a78f742a65421df26e8a782ec9c4d18877c6a30
sidramwaseem <sidraw24@gmail.com>
2022-06-02 23:32:50 +0500

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,11 +3,36 @@ import Task from "../Task/Task.jsx";
 import "./TaskList.css";
 
 function TaskList(props) {
+
+  const [checked, setChecked] = React.useState([]);
+
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+    
+  };
+
+  var isChecked = (item) =>
+  checked.includes(item) ? "checked-item" : "not-checked-item";
+ 
+
   return (
     <div className="tasklist-container">
       <div className="tasklist-conatiner_tasklist">
         {props.task.map((task) => (
-          <Task key={task.key} text={task.text} id={task.id} checked={props.checked} />
+          <Task
+          task={props.task}
+            key={task.key}
+            text={task.text}
+            id={task.id}
+            handleCheck={handleCheck}
+            isChecked={isChecked}
+          />
         ))}
       </div>
     </div>
