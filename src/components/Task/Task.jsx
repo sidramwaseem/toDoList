d77e618408a2cf1bea27ed3936b2fb856532da49
sidramwaseem <sidraw24@gmail.com>
2022-06-02 23:32:20 +0500

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import DeleteIcon from "../../assets/delete-icon.png";
 import "./Task.css";
 
-
 function Task(props) {
-  const [checked,setChecked] = React.useState(false);
 
-  function onChecked(){
-    setChecked(prevState => !prevState)
-  }
 
   return (
     <div className="task-container">
       <div className="task-container_check">
-        <input type="checkbox" name="checkbox" id={props.id} checked={checked} onChange={onChecked} />
-        <label htmlFor={props.id}>{props.text}</label>
+        <input
+          type="checkbox"
+          name="checkbox"
+          id={props.id}
+          onChange={props.handleCheck}
+        />
+        <label htmlFor={props.id} className={props.isChecked(props.task.text)}>{props.text}</label>
       </div>
       <img src={DeleteIcon} alt="delete-icon" className="delete-icon" />
     </div>
