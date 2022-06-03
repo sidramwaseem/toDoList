d1108abf3b5cea1751fdd4fef9275caa70198393
sidramwaseem <sidraw24@gmail.com>
2022-06-04 03:33:23 +0500

    Update Completed.jsx

--- src/components/Completed/Completed.jsx
+++ src/components/Completed/Completed.jsx
@@ -4,6 +4,7 @@ import TaskList from "../TaskList/TaskList.jsx";
 function Completed(props) {
   
 
+
   return (
     <div>{props.checkClass && <TaskList task={props.task} deleteTask={props.deleteTask} checkClass={props.checkClass}
     toggleClass={props.toggleClass} />}</div>
