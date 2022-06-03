bf01428e41058c41e88e76673fb0f02ae73ad4eb
sidramwaseem <sidraw24@gmail.com>
2022-06-03 13:47:36 +0500

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,20 +4,20 @@ import "./TaskList.css";
 
 function TaskList(props) {
  
-
+ 
+ 
+ 
   return (
     <div className="tasklist-container">
       <div className="tasklist-conatiner_tasklist">
-        {props.task.map((task,index) => (
+        {props.task.map((task, index) => (
           <Task
-          index={index}
-          task={props.task}
-            key={task.key}
+            task={props.task}
+            key={index}
             text={task.text}
             id={task.id}
             handleCheck={props.handleCheck}
-            isChecked={props.isChecked}
-            checked={props.checked}
+            deleteTask={props.deleteTask}
           />
         ))}
       </div>
