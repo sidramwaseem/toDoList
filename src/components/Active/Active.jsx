db667ccaac93ed773ec7f1c16cc5b0bc9b139495
sidramwaseem <sidraw24@gmail.com>
2022-06-03 13:46:48 +0500

    Update Active.jsx

--- src/components/Active/Active.jsx
+++ src/components/Active/Active.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Form from "../Form/Form.jsx";
-import TaskList from "../TaskList/TaskList.jsx"
+import TaskList from "../TaskList/TaskList.jsx";
 
 function Active(props) {
   return (
     <div className="Active-container">
       <Form task={props.task} addTask={props.addTask} addItem={props.addItem} />
-      <TaskList task={props.task} handleCheck={props.handleCheck}
-      isChecked={props.isChecked} checked={props.checked} />
+      {<TaskList task={props.task} handleCheck={props.handleCheck} deleteTask={props.deleteTask} />}
     </div>
   );
 }
