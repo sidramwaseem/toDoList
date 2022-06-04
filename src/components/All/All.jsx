b80a5dbd64d4774228bc156e9b0dd6929d379be3
sidramwaseem <sidraw24@gmail.com>
2022-06-05 00:41:27 +0500

    Update All.jsx

--- src/components/All/All.jsx
+++ src/components/All/All.jsx
@@ -4,12 +4,10 @@ import Form from "../Form/Form.jsx";
 import TaskList from "../TaskList/TaskList.jsx";
 
 function All(props) {
-
   return (
     <div className="All-container">
       <Form task={props.task} addTask={props.addTask} addItem={props.addItem} />
       <TaskList
-        isDone={props.isDone}
         task={props.task}
         handleCheck={props.handleCheck}
         deleteTask={props.deleteTask}
