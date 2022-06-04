993280e375cb766ad199e612503ee510efbb4a57
sidramwaseem <sidraw24@gmail.com>
2022-06-05 00:41:57 +0500

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,28 +3,29 @@ import DeleteIcon from "../../assets/delete-icon.png";
 import "./Task.css";
 
 function Task(props) {
-
-
   return (
     <div className="task-container">
       <div className="task-container_check">
         <input
           type="checkbox"
-          name="isDone"
           id={props.id}
-          onChange={props.handleCheck}
-          onClick={()=> props.toggleClass(props.id)}
           checked={props.checkClass}
+          onChange={props.handleCheck}
+          onClick={() => props.toggleClass(props.id)}
         />
         <label
           htmlFor={props.id}
-          value={props.checkClass}
           className={props.checkClass ? "checked-item" : "unchecked-item"}
         >
-          {props.text}
+          <span>{props.text}</span>
         </label>
       </div>
-      <img src={DeleteIcon} alt="delete-icon" className="delete-icon" onClick={()=>props.deleteTask(props.id)} />
+      <img
+        src={DeleteIcon}
+        alt="delete-icon"
+        className="delete-icon"
+        onClick={() => props.deleteTask(props.id)}
+      />
     </div>
   );
 }
