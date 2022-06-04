214385ab4083fe2a5d5bb597063ed3046d2d7d1a
sidramwaseem <sidraw24@gmail.com>
2022-06-04 15:31:00 +0500

    Update TaskList.jsx

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,10 +3,11 @@ import Task from "../Task/Task.jsx";
 import "./TaskList.css";
 
 function TaskList(props) {
+  const {task} = props
   return (
     <div className="tasklist-container">
       <div className="tasklist-conatiner_tasklist">
-        {props.task.map((task, index) => (
+        {task.map((task, index) => (
           <Task
           index={index}
             task={props.task}
