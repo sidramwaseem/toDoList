338be0d682b42d14e380833a72b2828c63166e82
sidramwaseem <sidraw24@gmail.com>
2022-06-08 11:31:46 +0500

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,19 +3,27 @@ import DeleteIcon from "../../assets/delete-icon.png";
 import "./Task.css";
 
 function Task(props) {
+
+  const [check, setCheck] = React.useState(false);
+  
+  function reverse(spec) {
+    let temp = spec.isCheck;
+    spec.isCheck = !temp;
+    setCheck(spec.isCheck);
+    console.log("xxxxxxxxxxxxxx", spec);
+  }
   return (
     <div className="task-container">
       <div className="task-container_check">
         <input
           type="checkbox"
           id={props.index}
-          checked={props.checkClass}
           onChange={props.handleCheck}
-          onClick={() => props.toggleClass(props.index)}
+          onClick={() => reverse(props.task)}
         />
         <label
           htmlFor={props.index}
-          className={props.checkClass ? "checked-item" : "unchecked-item"}
+          className={check ? "checked-item" : "unchecked-item"}
         >
           <span>{props.text}</span>
         </label>
